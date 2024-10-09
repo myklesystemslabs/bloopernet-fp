10cab36eb553d44ebad7faf5341bec6a9f9477dc
Mykle Hansen <mykle@mykle.com>
2024-10-09 15:06:45 -0700

    fix sound path

--- src/components/PatternSet.jsx
+++ src/components/PatternSet.jsx
@@ -42,7 +42,7 @@ const PatternSet = ({ dbName, beats, showNewTrackForm, onCancelNewTrack }) => {
           _id: id,
           type: 'instrument',
           name: instrument,
-          audioFile: `/public/sounds/${id}.wav`,
+          audioFile: `/sounds/${id}.wav`,
           mimeType: 'audio/wav',
           referenceType: 'url',
           createdAt: ts ? ts.now() : Date.now()
